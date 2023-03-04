52468605b1452b98275fad3d39e14993a98bbccb
kellynguyvn <80297074+kellynguyvn@users.noreply.github.com>
2023-03-03 17:04:48 -0800

    UI changes

---
+++ lib/widgets/onboarding_list.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class OnboardingList extends StatelessWidget {
+  final String description;
+  final IconData icone;
+  const OnboardingList({
+    Key? key,
+    required this.icone,
+    required this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Container(
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 150.0, bottom: 50.0),
+              child: Icon(
+                icone,
+                color: Colors.grey[400],
+              ),
+            ),
+            Text(
+              description,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[300]),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
